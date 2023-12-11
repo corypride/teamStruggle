74487e51ac21c06a4b878ab251b68a4a920eb7f5
amesp73 <amitrpatel502@gmail.com>
2023-12-10 21:32:39 -0600

    initial react search bar working

---
+++ react_frontend/src/Components/SearchBar.js
@@ -0,0 +1,66 @@
+import React, {useState} from 'react'
+import './SearchBar.css';
+import SearchIcon from '@mui/icons-material/Search';
+import CloseIcon from '@mui/icons-material/Close';
+
+function SearchBar({data}) {
+    const [filteredData, setFilteredData] = useState([]);
+    const [wordEntered, setWordEntered] = useState("");
+
+    const handleFilter = (event) => {
+        const searchWord = event.target.value;
+        setWordEntered(searchWord);
+        const newFilter = data.filter((value) => {
+            return value.Title.toLowerCase().includes(searchWord.toLowerCase());
+        });
+
+        if(searchWord === "") {
+            setFilteredData([]);
+        } else {
+            setFilteredData(newFilter);
+        }
+    }
+
+    const clearInput = () => {
+        setFilteredData([]);
+        setWordEntered("");
+    }
+
+    const handleClick = (value) => {
+        console.log(value.Title);
+    }
+
+    return (
+        <div className="search">
+            <div className="searchInput">
+                <input 
+                    type="text" 
+                    placeholder="Search for a movie" 
+                    value={wordEntered}
+                    onChange={handleFilter}
+                />
+                <div className="searchIcon">
+                    {filteredData.length === 0 ? (
+                        <SearchIcon /> 
+                    ) : (
+                        <CloseIcon id="clearBtn" onClick={clearInput}/>
+                    )}
+                </div>
+            </div>
+            {filteredData.length !== 0 && (
+            <div className="dataResults">
+                {filteredData.slice(0, 10).map((object, index) => {
+                    return (
+                    <div className='dataItem' onClick = {() => handleClick(object)}> 
+                        <p>{object.Title}</p> 
+                    </div>
+                    //div to a when link is present
+                    );
+                })}
+            </div>
+            )}
+        </div>
+    )
+    }
+
+export default SearchBar
