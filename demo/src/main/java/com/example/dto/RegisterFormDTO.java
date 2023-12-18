a99f8215c31d0b2119deddddba28e1c0c986b962
Marisa Starkweather <marisastarkweather@gmail.com>
2023-12-18 18:35:16 -0500

    more changes, added user dto

---
+++ demo/src/main/java/com/example/dto/RegisterFormDTO.java
@@ -0,0 +1,21 @@
+package main.java.com.example.models.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class RegisterFormDTO extends main.java.com.example.models.dto.LoginFormDTO {
+
+    @NotNull(message = "Password is required")
+    @NotBlank(message = "Password is required")
+    @Size(min = 7, max = 30, message = "Invalid password. Must be between 7 and 30 characters.")
+    private String verifyPassword;
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+    }
+}
