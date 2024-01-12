be6643942e8de767f6d49d600bc11a5817f49c5c
Gabe Breternitz <gabrielbreternitz@gmail.com>
2024-01-11 22:18:26 -0600

    created moviecard and watchlist, updated search to save a movie to the database

---
+++ react_frontend/src/Components/MovieCard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const MovieCard = ({ movie }) => {
+    return (
+        <div>
+            <div>
+                {movie.poster_path ? (
+                    <img
+                        src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+                        alt={`${movie.title} Art`}
+                    />
+                ) : (
+                    <div></div>
+                )}
+            </div>
+
+            <div>
+                <h3>{movie.title}</h3>
+            </div>
+        </div>
+    )
+}
--- react_frontend/src/pages/Search.js
+++ react_frontend/src/pages/Search.js
@@ -12,7 +12,12 @@ function Search() {
     let [results, setResults] = useState([]);
     let movies;
 
-    const handleClick = (aMovie) => {
+    const handleClick = async (aMovie) => {
+        //save movie to database
+        const response = await axios.post(`http://localhost:8080/movie`, aMovie);
+
+        //TODO: add movie to the specific watchlist selected
+        //--maybe this could be done by when you hover over the add to watchlist button, it shows you options?
         console.log(`Added ${aMovie.title} to watchlist!`);
     }
 
