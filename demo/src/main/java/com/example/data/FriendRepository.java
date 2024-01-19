cc4d58b6f1dddcc9ee3da8e96f0af0a470914d96
gbreternitz <gabrielbreternitz@gmail.com>
2024-01-18 19:55:47 -0600

    updated FriendRepository to match new friend class format

--- demo/src/main/java/com/example/data/FriendRepository.java
+++ demo/src/main/java/com/example/data/FriendRepository.java
@@ -2,17 +2,12 @@ package com.example.data;
 
 import com.example.models.Friend;
 import org.springframework.data.repository.CrudRepository;
-import com.example.models.User;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface FriendRepository extends CrudRepository<Friend,Integer> {
-
-    boolean existsByFirstUserAndSecondUser(User first,User second);
-
-    List<Friend> findByFirstUser(User user);
-    List<Friend> findBySecondUser(User user);
+    List<Friend> findByFriendId(Integer friendId);
 
 }
